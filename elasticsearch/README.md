e234c920b83ecd29771876c22640fae65f8ac19d
wesley2012 <wesley.mail@qq.com>
2017-07-25 10:35:48 +0800

    re-commit

--- elasticsearch/README.md
+++ elasticsearch/README.md
@@ -1,1 +1,2 @@
 * es_clear.sh - Clear an index type(清空一个索引类型)
+*requires jq, to install: apt install jq || yum install jq*
--- elasticsearch/es_clear.sh
+++ elasticsearch/es_clear.sh
@@ -6,17 +6,19 @@ type=$2
 
 test -z "$type" && echo "usage: $0 <index> <type>" && exit 1
 
-loop=true
-while $loop
+tmp=$(mktemp /tmp/es_clear_XXXXXX)
+
+while true
 do
 	loop=false
 	curl -d '{"fields":[]}' $prefix/$index/$type/_search?size=20 |
 		jq . |
-		sed -n 's/^.*"_id": "\([0-9]*\)",/\1/p' |
-		while read id
-		do
-			loop=true
-			curl -XDELETE $prefix/$index/$type/$id
-		done
+		sed -n 's/^.*"_id": "\([0-9]*\)",/\1/p' > $tmp
+	test -s $tmp || break
+	while read id
+	do
+		curl -XDELETE $prefix/$index/$type/$id
+	done < $tmp
 done
 
+/bin/rm $tmp
